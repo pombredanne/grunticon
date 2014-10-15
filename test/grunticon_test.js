075db34d510e0560b47270685ed7aa1e5d39b42e
Jeffrey Lembeck <jlembeck@gmail.com>
2014-10-15 07:48:03 -0700

    Push up loader for grunticon

--- test/grunticon_test.js
+++ test/grunticon_test.js
@@ -32,7 +32,8 @@ exports['grunt-grunticon'] = {
 
 		var actual = grunt.file.read('example/output/grunticon.loader.js');
 		var expected = grunt.file.read('test/expected/grunticon.loader.js');
-		test.equal(actual, expected, 'loader should match properly');
+		// adding this newline because the generated file doesn't end with one
+		test.equal(actual + "\n", expected, 'loader should match properly');
 
 		test.done();
 	}
